313f69eb30ad5a90eb9ab9589e5995326b6f42ca
Annabelle Wiegart <annabelle.wiegart@zb.uzh.ch>
2021-01-29 17:09:38 +0100

    ZB Ressourcen in 2 Windows

--- src/index.js
+++ src/index.js
@@ -15,42 +15,31 @@ const config = {
         ],
     },
     windows: [{
-            // No viewing hint provided; use window: defaultView
-            manifestId: 'https://purl.stanford.edu/fr426cg9537/iiif/manifest',
+            // viewingHint: paged --> automatically selects Book view
+            manifestId: 'https://www.e-manuscripta.ch/i3f/v20/2886898/manifest',
+            sideBarOpen: true,
+            canvasIndex: 7
         },
         {
-            // viewingHint: continuous --> automatically selects Scroll view
-            manifestId: 'https://iiif.library.ucla.edu/ark%3A%2F21198%2Fzz001nwthk/manifest',
+            // single view
+            manifestId: 'https://www.e-rara.ch/i3f/v20/21681782/manifest',
+            sideBarOpen: true,
         },
-        { // viewingHint: paged --> automatically selects Book view
-            manifestId: 'https://wellcomelibrary.org/iiif/b18035723/manifest',
-            canvasIndex: 1, // 
-        },
-        {
-            // viewingHint: paged --> Manually set to Gallery view
-            manifestId: 'https://iiif.bodleian.ox.ac.uk/iiif/manifest/e32a277e-91e2-4a6d-8ba6-cc4bad230410.json',
-            view: 'gallery',
-        }
     ],
-    catalog: [{ // These manifests are available in the catalog. 
-        //     manifestId: 'https://damsssl.llgc.org.uk/iiif/2.0/1556378/manifest.json',
-        //     provider: 'The National Library of Wales',
-        // },
-        // {
-        //     manifestId: 'https://damsssl.llgc.org.uk/iiif/2.0/1487439/manifest.json',
-        // },
+    catalog: [// These manifests are available in the catalog. 
         {
             manifestId: 'https://cdm16866.contentdm.oclc.org/iiif/info/cchm_photo/5342/manifest.json',
             provider: 'Washington State University',
         },
-        // { // Secret cat on the 76th page
-        //     manifestId: 'https://iiif.bodleian.ox.ac.uk/iiif/manifest/faeff7fb-f8a7-44b5-95ed-cff9a9ffd198.json',
-        //     provider: 'Bodleian Libraries, University of Oxford',
-        // },
+        { // Secret cat on the 76th page
+            manifestId: 'https://iiif.bodleian.ox.ac.uk/iiif/manifest/faeff7fb-f8a7-44b5-95ed-cff9a9ffd198.json',
+            provider: 'Bodleian Libraries, University of Oxford',
+        },
         {
             manifestId: 'https://www.e-codices.unifr.ch/metadata/iiif/collection.json',
             provider: 'e-codices',
         }
+    
     ],  
     selectedTheme: 'ZBTheme',
     themes:  {
