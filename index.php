c4dc5cc9682d7f8643c45a141e7b42f0e3a1d745
zimj <jan.zimmermann@zb.uzh.ch>
2021-06-03 11:12:58 +0200

    use php file, add plugin

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
     <title>Basic Mirador</title>
   </head>
   <body>
+    <?php 
+      echo "<div>moin</div>";
+    ?>
     <div id="mirador-viewer"></div>
     <script src="/dist/main.js"></script>
   </body>
