c9a6e83a7439b0f3d6d1475407ee78dde1ca36ab
zimj <jan.zimmermann@zb.uzh.ch>
2021-06-03 14:15:48 +0200

    fixed plugin

--- src/plugins/example_plugin.js
+++ src/plugins/example_plugin.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 class ExamplePlugin extends Component {
   render() {
     console.log('Plugin rendered');
+    
     return (
       <div>
         Hello World
