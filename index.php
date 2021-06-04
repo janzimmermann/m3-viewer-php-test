c2c11d947cd440836075ba620be342db71a819bc
Annabelle Wiegart <annabelle.wiegart@zb.uzh.ch>
2021-06-04 10:32:12 +0200

    Test if form data is written to file

--- index.php
+++ index.php
@@ -6,12 +6,12 @@
   <body>
     <?php 
       // echo "<div>moin</div>";
-      // $data = htmlspecialchars($_POST["collection"])
-      $data = file_get_contents('collections/sample_detektiiif_output.json');
+      $data = htmlspecialchars($_POST["collection"])
+      // $data = file_get_contents('collections/sample_detektiiif_output.json');
       // echo "<div>" . $div . "</div>";
       // echo "<div>" . htmlspecialchars($_POST["collection"]) . "</div>"
       $file = 'collections/myFabulousCollection.json';
-      file_put_contents ($file , $data);
+      file_put_contents($file , $data);
     ?>
     <div id="mirador-viewer"></div>
     <script src="/dist/main.js"></script>
