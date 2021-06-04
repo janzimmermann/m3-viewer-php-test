0af0c9f1f16558e010f233657ee31d8096cd584b
Annabelle Wiegart <annabelle.wiegart@zb.uzh.ch>
2021-06-04 07:53:08 +0200

    Save sample detektiiif collection to a JSON file

--- index.php
+++ index.php
@@ -5,8 +5,13 @@
   </head>
   <body>
     <?php 
-      echo "<div>moin</div>";
-       echo "<div>" . htmlspecialchars($_POST["collection"]) . "</div>"
+      // echo "<div>moin</div>";
+      // $data = htmlspecialchars($_POST["collection"])
+      $data = file_get_contents('collections/sample_detektiiif_output.json');
+      // echo "<div>" . $div . "</div>";
+      // echo "<div>" . htmlspecialchars($_POST["collection"]) . "</div>"
+      $file = 'collections/myFabulousCollection.json';
+      file_put_contents ($file , $data);
     ?>
     <div id="mirador-viewer"></div>
     <script src="/dist/main.js"></script>
